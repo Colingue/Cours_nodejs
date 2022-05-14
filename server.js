c4d4cd73c1d1bd0cbbeb23acb0267d295f786788
Colin Noiret <colin.noiret@gmail.com>
2022-05-14 22:58:49 +0200

    Ajout css recherche

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const hbs = create({ extname: ".hbs" });
 app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 
+
+app.use(express.static('public'));
+
 app.use(
   express.urlencoded({
     extended: false,
